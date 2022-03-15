e29d60dd1addaf849ee1064812056a26d5e23b85
mayo <smayorgac@gmail.com>
2022-03-14 21:04:01 -0300

    Primer commit
    blablabla

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("input your guess!");
+        println!(" (secret# is: [{}])", secret);
+        
+        let mut guess = String::new();
+        
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+        
+        let guess: u32 = match guess
+        .trim()
+        .parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("invalid input!...");
+                continue;
+            },
+        }; 
+        
+        println!("you guessed [{}]", guess);
+        
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("Yay! you win!");
+                break;
+            },
+        }
+    }
+}
